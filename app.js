5dc0bd158fc74ecb36a571e342cfc1278aef2d56
Allison Shultes <allison.shultes@bbc.co.uk>
2019-11-13 21:13:56 -0500

    [refactor]: lints code and modifies app.js

--- app.js
+++ app.js
@@ -1,36 +1,35 @@
-const express = require("express");
+const express = require('express');
+
 const app = express();
-const bodyparser = require("body-parser");
-const reversionScript = require("./reversion.js");
+const bodyparser = require('body-parser');
+const reversionScript = require('./reversion.js');
 
 const port = 8080;
 
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({ extended: false }));
 
-app.get('/status', async (req, res) => {
-    res.status(200).json({
-        message: "OK"
-    });
-})
+app.get('/status', async (req, res) => res.status(200).json({
+  message: 'OK',
+}));
 
-app.post("/reversion", async (req, res) => {
-    const script = req.body;
-    try {
-        const response = await reversionScript.reversionScript(script);
-        res.status(200).json({
-            response,
-        })
-    }
-    catch(e) {
-        throw e;
-    }
+app.post('/reversion', async (req, res) => {
+  const script = req.body;
+  try {
+    const response = await reversionScript.reversionScript(script);
+    res.status(200).json({
+      response,
+    });
+  } catch (e) {
+    res.status(500);
+    return e;
+  }
 });
 
 app.listen(port, () => {
-    console.log(`running at port ${port}`);
+  console.log(`running at port ${port}`);
 });
 
 module.exports = {
-    app
-}
+  app,
+};
--- tests/index.test.js
+++ tests/index.test.js
@@ -1,96 +1,96 @@
 const { expect } = require('chai');
-const reversion = require('../reversion')
+const reversion = require('../reversion');
 
 describe('Reversioning script', () => {
-    it('stripScript: should strip blank lines from the script', async () => {
-        const testScript = [
-            '',
-            'This is a test line',
-            '',
-            '',
-            'This is a test line'
-        ];
-        const testResult = [
-            'This is a test line',
-            'This is a test line'
-        ]
-        const result = reversion.stripScript(testScript);
-        expect(result).to.deep.equal(testResult);
-        expect(result).to.have.length(2);
-    });
+  it('stripScript: should strip blank lines from the script', async () => {
+    const testScript = [
+      '',
+      'This is a test line',
+      '',
+      '',
+      'This is a test line',
+    ];
+    const testResult = [
+      'This is a test line',
+      'This is a test line',
+    ];
+    const result = reversion.stripScript(testScript);
+    expect(result).to.deep.equal(testResult);
+    expect(result).to.have.length(2);
+  });
 
-    it('removeIndex: should delete index field from the script', () => {
-        const testScript = {
-            script: [
+  it('removeIndex: should delete index field from the script', () => {
+    const testScript = {
+      script: [
+        {
+          section: 'test section',
+          index: 10,
+          shots: [
             {
-                section: 'test section',
-                index: 10,
-                shots: [
-                {
-                    meta: [],
-                    index: 27,
-                    english: [],
-                    farsi: []
-                },
-                {
-                    meta: [],
-                    index: 29,
-                    english: [],
-                    farsi: []
-                }
-                ]
-                }, {
-                    section: 'test section',
-                    index: 11,
-                    shots: [
-                        {
-                            meta: [],
-                            index: 31,
-                            english: [],
-                            farsi: []
-                        },
-                        {
-                            meta: [],
-                            index: 34,
-                            english: [],
-                            farsi: []
-                        }
-                    ]
-                }
-        ]
+              meta: [],
+              index: 27,
+              english: [],
+              farsi: [],
+            },
+            {
+              meta: [],
+              index: 29,
+              english: [],
+              farsi: [],
+            },
+          ],
+        }, {
+          section: 'test section',
+          index: 11,
+          shots: [
+            {
+              meta: [],
+              index: 31,
+              english: [],
+              farsi: [],
+            },
+            {
+              meta: [],
+              index: 34,
+              english: [],
+              farsi: [],
+            },
+          ],
+        },
+      ],
     };
-        const result = reversion.removeIndex(testScript);
-        expect(result.script[0]).to.not.have.deep.property('index');
-        expect(result.script[1]).to.not.have.deep.property('index');
-    });
+    const result = reversion.removeIndex(testScript);
+    expect(result.script[0]).to.not.have.deep.property('index');
+    expect(result.script[1]).to.not.have.deep.property('index');
+  });
 
-    it('findShot: should return proper object', () => {
-        const testScript = {
-            script: [
-                {
-                    section: 'test section',
-                    shots: [
-                        {
-                            meta: [],
-                            index: 27,
-                            english: [],
-                            farsi: []
-                        },
-                        {
-                            meta: [],
-                            index: 29,
-                            english: [],
-                            farsi: []
-                        }
-                    ]
-                }
-            ]
-        };
-        const firstTestIndex = 28;
-        const secondTestIndex = 32;
-        const firstResult = reversion.findShot(testScript.script[0], firstTestIndex);
-        expect(firstResult).to.deep.equal(testScript.script[0].shots[0]);
-        const secondResult = reversion.findShot(testScript.script[0], secondTestIndex);
-        expect(secondResult).to.deep.equal(testScript.script[0].shots[1]);
-    })
-})
+  it('findShot: should return proper object', () => {
+    const testScript = {
+      script: [
+        {
+          section: 'test section',
+          shots: [
+            {
+              meta: [],
+              index: 27,
+              english: [],
+              farsi: [],
+            },
+            {
+              meta: [],
+              index: 29,
+              english: [],
+              farsi: [],
+            },
+          ],
+        },
+      ],
+    };
+    const firstTestIndex = 28;
+    const secondTestIndex = 32;
+    const firstResult = reversion.findShot(testScript.script[0], firstTestIndex);
+    expect(firstResult).to.deep.equal(testScript.script[0].shots[0]);
+    const secondResult = reversion.findShot(testScript.script[0], secondTestIndex);
+    expect(secondResult).to.deep.equal(testScript.script[0].shots[1]);
+  });
+});
